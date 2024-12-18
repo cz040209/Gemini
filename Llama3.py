047fc7bf3753afa50243d843c71e50adb422a820
cz040209 <leecz-wm23@student.tarc.edu.my>
2024-12-19 00:06:29 +0800

    Create Llama3.py

---
+++ Llama3.py
@@ -0,0 +1,25 @@
+import os
+from azure.ai.inference import ChatCompletionsClient
+from azure.ai.inference.models import SystemMessage
+from azure.ai.inference.models import UserMessage
+from azure.core.credentials import AzureKeyCredential
+
+# To authenticate with the model you will need to generate a personal access token (PAT) in your GitHub settings. 
+# Create your PAT token by following instructions here: https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/managing-your-personal-access-tokens
+client = ChatCompletionsClient(
+    endpoint="https://models.inference.ai.azure.com",
+    credential=AzureKeyCredential(os.environ["GITHUB_TOKEN"]),
+)
+
+response = client.complete(
+    messages=[
+        SystemMessage(content=""""""),
+        UserMessage(content="What is Data Science?"),
+    ],
+    model="Meta-Llama-3.1-70B-Instruct",
+    temperature=0.8,
+    max_tokens=2048,
+    top_p=0.1
+)
+
+print(response.choices[0].message.content)
